94c39f1ce59c93b9b4912e1ff280b30dada5e541
Johnny Estilles <johnny.estilles@agentia.asia>
2015-03-17 01:18:17 +0800

    updated test and fixed lint

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/pingHandler');
+module.exports = require('./lib/pingHandler')();
--- test/lib/config.test.js
+++ test/lib/config.test.js
@@ -1,48 +1,48 @@
-var _ = require('lodash'),
-	chai = require('chai'),
+var chai = require('chai'),
 	expect = chai.expect,
 	sinon = require('sinon'),
 	sinonChai = require('sinon-chai'),
-	ping = require('../../')();
+	pingHandler = require('../../');
 
 chai.use(sinonChai);
 
 describe('.config()', function () {
 
 	before(function () {
-		sinon.stub(ping, '__error');
-		sinon.stub(ping, 'set');
+		sinon.stub(pingHandler, 'set');
 	});
 
 	after(function() {
-		ping.__error.restore();
-		ping.set.restore();
+		pingHandler.set.restore();
 	});
 
 	describe('with invalid arguments', function () {
 
-		afterEach(function () {
-			ping.__error.reset();
+		afterEach(function() {
+			pingHandler.set.reset();
 		});
 
 		it('should error when called with no arguments', function() {
-			ping.config();
-			expect(ping.__error).to.have.been.called.once;
-			expect(ping.__error).to.have.been.calledWithExactly('Invalid configuration settings. Must be an object.', 'TypeError');
+			var fn = function() {
+				pingHandler.config();
+			};
+			expect(fn).to.throw('Invalid configuration settings. Must be an object.');
 		});
 
 		it('should error when called with a non-object', function() {
-			ping.config('invalid');
-			expect(ping.__error).to.have.been.called.once;
-			expect(ping.__error).to.have.been.calledWithExactly('Invalid configuration settings. Must be an object.', 'TypeError');
+			var fn = function() {
+				pingHandler.config('invalid');
+			};
+			expect(fn).to.throw('Invalid configuration settings. Must be an object.');
 		});
 
 		it('should error when called with an invalid option', function() {
-			ping.config({
-				'invalid': 'value'
-			});
-			expect(ping.__error).to.have.been.called.once;
-			expect(ping.__error).to.have.been.calledWithExactly('Invalid configuration settings. "invalid" is not a valid setting.', 'ConfigError');
+			var fn = function() {
+				pingHandler.config({
+					'invalid': 'value'
+				});
+			};
+			expect(fn).to.throw('Invalid configuration settings. "invalid" is not a valid setting.');
 		});
 
 	});
@@ -54,10 +54,10 @@ describe('.config()', function () {
 				'path': '/valid/path'
 			};
 
-			ping.config(options);
-			expect(ping.set).to.be.called.once;
-			expect(ping.set).to.have.been.calledWithExactly('path', '/valid/path');
-			ping.set.reset();
+			pingHandler.config(options);
+			expect(pingHandler.set).to.be.called.once;
+			expect(pingHandler.set).to.have.been.calledWithExactly('path', '/valid/path');
+			pingHandler.set.reset();
 
 			options = {
 				'path': '/valid/path',
@@ -65,11 +65,11 @@ describe('.config()', function () {
 				'payload': 'GOOD'
 			};
 
-			ping.config(options);
-			expect(ping.set).to.be.called.thrice;
-			expect(ping.set.getCall(0)).to.have.been.calledWithExactly('path', '/valid/path');
-			expect(ping.set.getCall(1)).to.have.been.calledWithExactly('methods', ['GET', 'HEAD']);
-			expect(ping.set.getCall(2)).to.have.been.calledWithExactly('payload', 'GOOD');
+			pingHandler.config(options);
+			expect(pingHandler.set).to.be.called.thrice;
+			expect(pingHandler.set.firstCall).to.have.been.calledWithExactly('path', '/valid/path');
+			expect(pingHandler.set.secondCall).to.have.been.calledWithExactly('methods', ['GET', 'HEAD']);
+			expect(pingHandler.set.thirdCall).to.have.been.calledWithExactly('payload', 'GOOD');
 		});
 
 	});
